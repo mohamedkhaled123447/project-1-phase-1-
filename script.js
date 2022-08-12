0dd023f43eb9e269111513c652cb669f60ad4e8f
mohamed khaled <moham35356@gmail.com>
2022-08-12 22:57:14 +0200

    update in js file

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 let courses;
-window.onload = () => {
-  fetch("http://myjson.dit.upm.es/api/bins/95hg")
-    .then((response) => response.json())
-    .then((json) => {
-      courses = json;
-      for (let i = 0; i < courses.length; i++) {
-        let div = document.createElement("div");
-        div.className = "card";
-        let image = document.createElement("img");
-        let a = document.createElement("a");
-        let author_span = document.createElement("span");
-        let price_span = document.createElement("span");
-        author_span.className = "author";
-        price_span.className = "price";
-        author_span.appendChild(document.createTextNode(courses[i].author));
-        price_span.innerHTML = "&#69;&#163;";
-        price_span.appendChild(document.createTextNode(courses[i].price));
-        image.src = courses[i].image;
-        a.href = courses[i].link;
-        a.appendChild(image);
-        a.appendChild(document.createTextNode(courses[i].title));
-        a.appendChild(author_span);
-        a.appendChild(price_span);
-        div.append(a);
-        document.querySelector(".cards").appendChild(div);
-      }
-    });
-};
+
+fetch("http://myjson.dit.upm.es/api/bins/95hg")
+  .then((response) => response.json())
+  .then((json) => {
+    courses = json;
+    for (let i = 0; i < courses.length; i++) {
+      let div = document.createElement("div");
+      div.className = "card";
+      let image = document.createElement("img");
+      let a = document.createElement("a");
+      let author_span = document.createElement("span");
+      let price_span = document.createElement("span");
+      author_span.className = "author";
+      price_span.className = "price";
+      author_span.appendChild(document.createTextNode(courses[i].author));
+      price_span.innerHTML = "&#69;&#163;";
+      price_span.appendChild(document.createTextNode(courses[i].price));
+      image.src = courses[i].image;
+      a.href = courses[i].link;
+      a.appendChild(image);
+      a.appendChild(document.createTextNode(courses[i].title));
+      a.appendChild(author_span);
+      a.appendChild(price_span);
+      div.append(a);
+      document.querySelector(".cards").appendChild(div);
+    }
+  });
 document.querySelector(".form-button").addEventListener("click", (event) => {
   event.preventDefault();
 });
